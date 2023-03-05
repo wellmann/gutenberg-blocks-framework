7ae46b9cec69a8ca3146e000a14eea91978ff979
Kevin Wellmann <kevin@wellmann.io>
2023-03-05 11:09:26 +0100

    fix tests

--- tests/AssetCollectorTest.php
+++ tests/AssetCollectorTest.php
@@ -7,7 +7,6 @@ namespace KWIO\GutenbergBlocks\Tests;
 use KWIO\GutenbergBlocks\AssetCollector;
 use KWIO\GutenbergBlocks\PluginConfigDTO;
 use ReflectionClass;
-use stdClass;
 
 use function Brain\Monkey\Functions\expect;
 use function Brain\Monkey\Functions\when;
@@ -63,52 +62,22 @@ class AssetCollectorTest extends TestCase
             ->once()
             ->with('prefix-blocks-editor', '/dist/editor.js', [], '', true);
 
+        expect('wp_set_script_translations')
+            ->once()
+            ->with('prefix-blocks-editor', 'prefix', '');
+
         $assetCollector = new AssetCollector($this->pluginConfig);
         $assetCollector->enqueueEditorAssets();
     }
 
     public function testEnqueueScripts()
     {
-        $restUrl = html_entity_decode(json_encode('Url'), ENT_QUOTES, 'UTF-8');
         when('get_rest_url')->justReturn('Url');
         expect('wp_enqueue_script')
             ->once()
             ->with('prefix-blocks', '/dist/blocks.js', [], '', true);
-        expect('wp_add_inline_script')
-            ->once()
-            ->with('prefix-blocks', "var Prefix = Prefix || {};\nPrefix.apiRoot = {$restUrl};", 'before');
 
         $assetCollector = new AssetCollector($this->pluginConfig);
         $assetCollector->enqueueScripts();
-    }
-
-    public function testEnqueueEditorTranslations()
-    {
-        when('is_readable')->justReturn(true);
-        when('file_get_contents')->justReturn('localeData');
-
-        $domain = $this->pluginConfig->prefix;
-        $localeData = 'localeData';
-
-        expect('wp_add_inline_script')
-            ->once()
-            ->with(
-                'prefix-blocks-editor',
-                <<<JS
-( function( domain, translations ) {
-    var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
-    localeData[""].domain = domain;
-    wp.i18n.setLocaleData( localeData, domain );
-} )( "{$domain}", {$localeData} );
-JS,
-                'before'
-            );
-
-        $assetCollector = new AssetCollector($this->pluginConfig);
-        $assetCollectorReflection = new ReflectionClass($assetCollector);
-
-        $blockCollectorEnqueueEditorTranslations = $assetCollectorReflection->getMethod('enqueueEditorTranslations');
-        $blockCollectorEnqueueEditorTranslations->setAccessible(true);
-        $blockCollectorEnqueueEditorTranslations->invoke($assetCollector);
     }
 }
